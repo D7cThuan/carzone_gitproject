3947c630a638b77423f54b5af49d23754651c2b4
D7cThuan <knownleage@gmail.com>
2021-10-15 14:10:55 +0700

    Hoan thanh team model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'carzone_db',
+         'USER': 'postgres',
+         'PASSWORD': 'Th09092000',
+         'HOST': 'localhost',
+     }
+ }
 
 
 # Password validation
@@ -123,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
