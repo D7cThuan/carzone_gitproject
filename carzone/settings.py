07868ed3a75c50251631ee88e6bf9a1b62cb6d0d
D7cThuan <76238671+D7cThuan@users.noreply.github.com>
2021-11-09 14:01:17 +0700

    allowedhost

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aqueous-crag-20070.herokuapp.com', 'localhost', '127.0.0.1', 'pure-garden-23884.herokuapp.com']
+ALLOWED_HOSTS = ['aqueous-crag-20070.herokuapp.com', 'localhost', '127.0.0.1','31.220.53.16', 'ckcthuexe.store', 'pure-garden-23884.herokuapp.com']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
